71d347385faae06c6ce3585852d5c495f5bf08ca
tackin <tackin@mail2.digital-layout.de>
2017-06-28 21:18:07 +0200

    includes the trigger
    This is for the 2. FW-Build ONLY. It triggers the switching when ever we
    have a fastd-uplink.

---
+++ files/lib/gluon/change_mesh.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+meshSSID_prefix="d2:84:2a:f7"
+host=`uname -n`
+okt0=${host:0:2}
+okt1=${host:2:1}"0"
+suffix=$okt0":"$okt1
+if [ `echo $suffix | egrep "^([0-9A-Fa-f]{2}:){1}[0-9A-Fa-f]{2}$"` ];
+then
+    # valide MAC
+	newIBSS=$meshSSID_prefix":"$suffix
+else
+  # invalide MAC
+  newIBSS=$meshSSID_prefix":0:0"
+fi
+if [ $(batctl gwl | grep "=>" | grep -c mesh-vpn) -gt 0 ]; 
+  then
+    # Node has an uplink and switches its mesh
+	uci set wireless.ibss_radio0.ssid=$newIBSS
+	uci set wireless.ibss_radio0.bssid=$newIBSS
+	wifi
+  else
+  if [ $(iw ibss0 scan | grep -c "$newIBSS") -gt 1 ];
+    then
+    # new mesh found
+	uci set wireless.ibss_radio0.ssid=$newIBSS
+	uci set wireless.ibss_radio0.bssid=$newIBSS
+	wifi
+  fi
+fi
